19f691ac2e2365d4905e576f21a47a205f23fe76
Alexei Antipov <antipov.alexei@gmail.com>
2017-08-17 10:48:24 +0300

    Define general spec for the <bowling> component

--- src/common/bowling/bowing.component.spec.js
+++ src/common/bowling/bowing.component.spec.js
@@ -12,4 +12,56 @@ describe('<bowling> component', () => {
       ctrl = _$componentController_('bowling', null, {});
     })
   );
+
+  describe('::$onInit()', () => {
+    beforeEach(() => {
+      ctrl.$onInit();
+    });
+
+    it('should define isGameStarted prop', () => {
+      expect(ctrl.isGameStarted).toBe(false);
+    });
+
+    it('should define isGameFinished prop', () => {
+      expect(ctrl.isGameFinished).toBe(false);
+    });
+
+    it('should define players prop', () => {
+      expect(ctrl.players).toEqual([]);
+    });
+
+    it('should define current player prop', () => {
+      expect(ctrl.player).toBeNull();
+    });
+
+    it('should define isGameFinished prop', () => {
+      expect(ctrl.isGameFinished).toBe(false);
+    });
+
+    it('should define current frame prop', () => {
+      expect(ctrl.frame).toBe(1);
+    });
+
+    it('should define current roll prop', () => {
+      expect(ctrl.roll).toBe(1);
+    });
+  });
+
+  describe('::addPlayer()', () => {
+    it('should be defined', () => {
+      expect(typeof ctrl.addPlayer).toBe('function');
+    });
+  });
+
+  describe('::start()', () => {
+    it('should be defined', () => {
+      expect(typeof ctrl.start).toBe('function');
+    });
+  });
+
+  describe('::doRoll()', () => {
+    it('should be defined', () => {
+      expect(typeof ctrl.doRoll).toBe('function');
+    });
+  });
 });
